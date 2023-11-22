532ac32dbda9c740732002baaf3daec8e6a903dc
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2023-11-22 17:32:43 +0530

    Update the project

---
+++ Interface/IOrder.cs
@@ -0,0 +1,16 @@
+﻿using GoogleFirebase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoogleFirebase.Interface
+{
+    public interface IOrder
+    {
+        public Task<List<Order>> GetAllOrders();
+        public void AddOrder(Order order);
+        public void UpdateOrder(Order order);
+        public void DeleteOrder(string orderId);
+    }
+}
